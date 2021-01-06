b8e6a1af33980e92cadb0ba8f52b548cde952a06
Rafael Vastag <rgvastag@gmail.com>
2021-01-06 19:18:14 -0300

    Routes

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css';
-import Home from './Home/Index';
 import Navbar from './Navbar';
+import Routes from './Routes';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Home />
-    </div>
+    
+      <Routes />
+
   );
 }
 
---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import { ReactComponent as Logo } from './logo.svg';
+
+function Orders() {
+    return (
+        <div>
+            <h1>Orders</h1>
+        </div>
+    );
+}
+
+export default Orders;
