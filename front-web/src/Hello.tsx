327f9a6abae797b6a14f924edbdd17e20b400bf6
Rafael Vastag <rgvastag@gmail.com>
2021-01-06 12:37:29 -0300

    Use effect

--- front-web/src/Hello.tsx
+++ front-web/src/Hello.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
+
 type Props = {
     message: string;
 }
 
 function Hello( {message} : Props) {
+
+    useEffect(() => {
+        console.log('Comp Iniciou')
+    }, []);
+
     return (
         <h1>Hello { message } ! </h1>
     )
