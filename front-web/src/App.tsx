c4ffd852ff9489c0dc6975a2c8b1418a7deabdc3
Rafael Vastag <rgvastag@gmail.com>
2021-01-06 18:03:41 -0300

    Config FrontWeb

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,9 @@
 import './App.css';
-import Counter from './Counter';
-import Hello from './Hello';
 
 function App() {
   return (
     <div>
-      <Counter></Counter>
-      <Hello message = "Rafael" />
-      <Hello message = "Wagner" />
-      <Hello message = "Cecilia" />
-      <Hello message = "Brasil" />
+      Hello
     </div>
   );
 }
