366b5c44296517d5172c51093f62153c5eb7befa
Rafael Vastag <rgvastag@gmail.com>
2021-01-10 19:53:55 -0300

    orderLocation

---
+++ front-web/src/Orders/OrderLocation.tsx
@@ -0,0 +1,16 @@
+function OrderLocation() {
+    return (
+        <div className="order-location-container">
+            <div className= "order-location-content">
+                <h3 className="order-location-title">
+                    Selecione o endereço de entrega:
+                </h3>
+                <div className="filter-container">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default OrderLocation;
